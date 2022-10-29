aa65eb2f03a1d0021a217f2416d2210146552825
Jarvis0p <krishnajaiswal.2208@gmail.com>
2022-10-30 05:14:40 +0530

    completed booked ticket insertion

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get("/movies", function (req, res) {
 });
 
 app.get("/book-ticket/:movieNo", function (req, res) {
-    mysqlQuery = "select sql7530125.show.SlotNo,sql7530125.show.Time,movie.MovieName,movie.Language,movie.Duration,movie.Img,movie.Description,theater.TheaterName from sql7530125.show, movie, theater where sql7530125.show.MovieNo = movie.MovieNo  and sql7530125.show.TheaterNo = theater.TheaterNo and movie.MovieNo ="+req.params.movieNo
+    mysqlQuery = "select sql7530125.show.SlotNo,sql7530125.show.Time,movie.MovieName,movie.Language,movie.Duration,movie.Img,movie.Description,theater.TheaterName from sql7530125.show, movie, theater where sql7530125.show.MovieNo = movie.MovieNo  and sql7530125.show.TheaterNo = theater.TheaterNo and movie.MovieNo =" + req.params.movieNo
     connections.query(mysqlQuery, function (err, result, fields) {
         if (err) throw err;
         result = JSON.parse(JSON.stringify(result));
@@ -77,23 +77,54 @@ app.get("/book-ticket/:movieNo", function (req, res) {
 });
 
 
-app.get("/book/:movie/:theater/:date/:time",function(req,res){
-    console.log(req.params.theater,req.params.date,req.params.time);
-    result = [
-        {   movie: req.params.movie,
-            theater: req.params.theater,
-            date: req.params.date,
-            time: req.params.time
-        }
-    ]
-    res.render("confirmation",{
-        data: result
+// app.get("/book/:movie/:theater/:date/:time",function(req,res){
+//     console.log(req.params.theater,req.params.date,req.params.time);
+//     result = [
+//         {   movie: req.params.movie,
+//             theater: req.params.theater,
+//             date: req.params.date,
+//             time: req.params.time
+//         }
+//     ]
+//     res.render("confirmation",{
+//         data: result
+//     });
+// });
+
+app.post("/book-ticket", function (req, res) {
+    mysqlQuery = "select sql7530125.show.SlotNo,sql7530125.show.Time,movie.MovieName,movie.Language,movie.Duration,movie.Img,movie.Description,theater.TheaterName from sql7530125.show, movie, theater where sql7530125.show.MovieNo = movie.MovieNo  and sql7530125.show.TheaterNo = theater.TheaterNo and sql7530125.show.SlotNo =" + req.body.SlotNo
+    connections.query(mysqlQuery, function (err, result, fields) {
+        if (err) throw err;
+        result = JSON.parse(JSON.stringify(result));
+        console.log(result);
+        res.render("confirmation", {
+            data: result,
+            date: req.body.date
+        });
     });
 });
 
-app.post("/book-ticket", function (req, res) {
+app.post("/details", function (req, res) {
     console.log(req.body);
+    insertQuery = "INSERT INTO `sql7530125`.`bookings` (`SlotNo`, `Name`, `Email`, `Date`) VALUES (\'" + req.body.SlotNo + "\', \'" + req.body.name + "\', \'" + req.body.name + "\', \'" + req.body.date + "\');"
+    connections.query(insertQuery, function (err, insertResult, fields) {
+        if (err) throw err;
+        console.log("Into INSERT");
+        console.log(insertResult.insertId);
+
+        mysqlQuery = "SELECT * FROM sql7530125.bookings where BookingNo="+insertResult.insertId
+        connections.query(mysqlQuery, function (err, result, fields) {
+            if (err) throw err;
+            result = JSON.parse(JSON.stringify(result));
+            res.render("thanku", {
+                data: result
+            });
+        });
+    });
 });
+
+
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000");
